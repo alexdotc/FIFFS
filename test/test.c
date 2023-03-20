177e90eaba1179b8c589477f687fd99608944ca6
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-03-20 10:31:38 -0400

    clang-format, close fd, and update to C11 timespec in test.c

--- test/test.c
+++ test/test.c
@@ -1,21 +1,22 @@
 #include <fcntl.h>
 #include <getopt.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
-typedef struct Options{
+typedef struct Options {
     uint64_t nfiles;
 } Options;
 
-static Options* parse_options(int argc, char *const argv[]);
+static Options *parse_options(int argc, char *const argv[]);
 
-int main(int argc, char *const argv[]){
+int main(int argc, char *const argv[]) {
 
-    Options* options = parse_options(argc, argv);
+    Options *options = parse_options(argc, argv);
     long nfiles = options->nfiles;
 
     char namebuf[64];
@@ -23,43 +24,48 @@ int main(int argc, char *const argv[]){
     struct timespec ts1;
     time_t sec;
     struct stat statbuf;
-    clock_gettime(CLOCK_REALTIME, &ts0);
+    timespec_get(&ts0, TIME_UTC);
 
-    for (int i = 0; i < nfiles; ++i){
+    for (int i = 0; i < nfiles; ++i) {
         sprintf(namebuf, "mop%d.f", i);
-        creat(namebuf, O_RDWR);
+        close(creat(namebuf, O_RDWR));
     }
 
-    clock_gettime(CLOCK_REALTIME, &ts1);
+    timespec_get(&ts1, TIME_UTC);
     sec = ts1.tv_sec - ts0.tv_sec;
 
-    printf("Took approx. %ld seconds to create %ld files\n", sec, nfiles); 
+    printf("Took approx. %ld seconds to create %ld files\n", sec, nfiles);
 
-    clock_gettime(CLOCK_REALTIME, &ts0);
+    timespec_get(&ts0, TIME_UTC);
 
-    for (int i = 0; i < nfiles; ++i){
+    for (int i = 0; i < nfiles; ++i) {
         sprintf(namebuf, "mop%d.f", i);
         stat(namebuf, &statbuf);
     }
 
-    clock_gettime(CLOCK_REALTIME, &ts1);
+    timespec_get(&ts1, TIME_UTC);
     sec = ts1.tv_sec - ts0.tv_sec;
 
-    printf("Took approx. %ld seconds to stat %ld files\n", sec, nfiles); 
+    printf("Took approx. %ld seconds to stat %ld files\n", sec, nfiles);
 
     return 0;
 }
 
-static Options* parse_options(int argc, char *const argv[]){
+static Options *parse_options(int argc, char *const argv[]) {
     Options *options = malloc(sizeof(Options));
-    if (options == NULL) exit(EXIT_FAILURE);
+    if (options == NULL)
+        exit(EXIT_FAILURE);
 
     int opt;
-    while((opt = getopt(argc, argv, "N:")) != -1)
-    switch(opt){
-        case 'N': options->nfiles = strtol(optarg, NULL, 0); break;
-        default: fprintf(stderr, "Usage: %s -N nfiles\n", argv[0]); exit(EXIT_FAILURE);
-    }
+    while ((opt = getopt(argc, argv, "N:")) != -1)
+        switch (opt) {
+        case 'N':
+            options->nfiles = strtol(optarg, NULL, 0);
+            break;
+        default:
+            fprintf(stderr, "Usage: %s -N nfiles\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
 
     return options;
 }
